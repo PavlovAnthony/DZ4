a27645e16ded9dbdab7b799e034cc3135cd3d9a4
Anton <pavlov@mail.ru>
2022-09-29 21:04:01 +0500

    Zadacha 25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,40 @@
+﻿string ReadData(string line)
+{
+
+    Console.Write(line);
+    string a = Console.ReadLine();
+    return a;
+}
+
+double  CheckNumber(string x)
+{
+    double num = 0;
+
+    bool isNum = double.TryParse(x, out num);
+    if (isNum == false)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        PrintData("Ошибка ввода: ", "значение Null либо введены не числа");
+        Console.ResetColor();
+        Environment.Exit(0);
+    }
+return num;
+}
+
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+double FindDegree(double a, double b)
+{
+
+    return Math.Pow(a, b);
+}
+
+double a = CheckNumber(ReadData("Введите число А, которое возводим в степень: "));
+double b = CheckNumber(ReadData("Введите число B - степень числа А: "));
+
+double result = FindDegree(a, b);
+PrintData("Число A  в степени B: ", result.ToString());
+
